1da94fef25864e598432a02ea6c4002a37eaaf02
Michael Squashic <msquashic@gmail.com>
2012-09-28 23:19:36 -0400

    thisisnotavirus.com v1.0

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>THISISNOTAVIRUS</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" title="no title">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+	<script src="js/animate01.js" type="text/javascript"></script>
+
+</head>
+<body>
+	<h1>THISISNOTAVIRUS</h1>
+</body>
+</html>
---
+++ js/animate01.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	screen_width = screen.width;
+	screen_height = screen.height;
+	$body = $('body');
+	$h1 = $('h1');
+
+	bgs = ['#000000', '#00ffbd', '#ffff00', '#bege76'];
+
+	$h1.css({
+		'left': ($('html').width()/2)-(747/2)+'px',
+		'top': ($('html').height()/2)-(62/2)+'px'
+	});
+
+	setInterval(function(){
+		x = Math.random()*screen_width;
+		y = Math.random()*screen_height;
+
+	  var html = '<div style="top:'+y+'px; left:'+x+'px;"></div>';
+
+	  var dot = document.createDocumentFragment();
+		dot.innerHTML = html;
+		$body.append(html);
+
+		// $body.append($dot);
+
+		document.title = x*y*538531294576739;
+
+		rand = Math.floor(Math.random()*6);
+
+		$h1.css('background-color', bgs[rand]);
+	}, 5);
+});
