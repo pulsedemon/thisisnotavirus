80e9f0ff61a9161556c65b14f2d9e92accc054f2
pulsedemon <msquashic@gmail.com>
2012-11-06 03:12:34 -0500

    cleanup

--- index.php
+++ index.php
@@ -10,41 +10,6 @@
 </head>
 <body>
 	<div id="container"></div>
-
-<script type="x-shader/x-vertex" id="vertexshader">
-  attribute float size;
-  attribute vec3 ca;
-
-  varying vec3 vColor;
-
-  void main() {
-
-    vColor = ca;
-
-    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-
-    //gl_PointSize = size;
-    gl_PointSize = size * ( 300.0 / length( mvPosition.xyz ) );
-
-    gl_Position = projectionMatrix * mvPosition;
-
-  }
-</script>
-
-<script type="x-shader/x-fragment" id="fragmentshader">
-  uniform vec3 color;
-  uniform sampler2D texture;
-
-  varying vec3 vColor;
-
-  void main() {
-
-    gl_FragColor = vec4( color * vColor, 1.0 );
-    gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );
-
-  }
-</script>
-
-<script src="js/animate02.js"></script>
+  <script src="js/animate02.js"></script>
 </body>
 </html>
