5aae62d83eb1b4703c5dea4fd4558e674d7a3e35
Michael Squashic <msquashic@gmail.com>
2022-04-09 22:38:24 -0400

    remove stupid waves

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const viruses = ["random-blocks", "uzumaki", "snow-storm", "flash", "waves"];
+// const viruses = ["random-blocks", "uzumaki", "snow-storm", "flash", "waves"];
+const viruses = ["random-blocks", "uzumaki", "snow-storm", "flash"];
 
 const random_times = [
   1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
