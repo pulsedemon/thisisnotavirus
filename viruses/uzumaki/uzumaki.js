0e0866437f04cfa8a5a30986a402700cd0d35670
Michael Squashic <msquashic@gmail.com>
2023-01-03 15:59:30 -0500

    move uzumaki js to html file

--- viruses/uzumaki/uzumaki.js
+++ viruses/uzumaki/uzumaki.js
@@ -1,5 +1,3 @@
 const colors = ["#ff0000", "#00ffff", "#ffffff"];
 const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
-
 document.body.style.backgroundColor = randomColor;
