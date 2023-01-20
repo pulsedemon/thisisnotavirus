ba2b13d646cd5a088d1f5bf1ebf09524a707dff5
Michael Squashic <msquashic@gmail.com>
2023-01-20 17:38:47 -0500

    refactor sphere.js

--- viruses/sphere/sphere.js
+++ viruses/sphere/sphere.js
@@ -2,115 +2,131 @@
 
 import * as THREE from "three";
 
-let WIDTH = window.innerWidth,
+class Sphere {
+  WIDTH = window.innerWidth;
   HEIGHT = window.innerHeight;
+  renderer;
+  scene;
+  camera;
+  vortex;
+  diamond;
+  dcCalls = 0;
+  randomizeSphereColor = false;
+  randomizeDiamondColor = false;
 
-let renderer, scene, camera, vortex, diamond;
-let $diamond_color;
+  constructor() {
+    if (this.HEIGHT > this.WIDTH) {
+      this.HEIGHT = this.WIDTH;
+    }
 
-function init() {
-  if (HEIGHT > WIDTH) {
-    HEIGHT = WIDTH;
-  }
+    let VIEW_ANGLE = 45,
+      ASPECT = this.WIDTH / this.HEIGHT,
+      NEAR = 1,
+      FAR = 10000;
 
-  let VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 1,
-    FAR = 10000;
+    this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 
-  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    this.camera.position.z = 300;
+    this.scene = new THREE.Scene();
 
-  camera.position.z = 300;
-  scene = new THREE.Scene();
+    this.WIDTH = this.WIDTH * 2;
+    this.HEIGHT = this.HEIGHT * 2;
 
-  WIDTH = WIDTH * 2;
-  HEIGHT = HEIGHT * 2;
-
-  let radius = 100,
-    segments = 35,
-    rings = 18;
-  let geometry = new THREE.SphereGeometry(
-    radius,
-    segments,
-    rings,
-    0,
-    Math.PI * 2,
-    0,
-    Math.PI * 2
-  );
-  let random_color = Math.random() * 0xffffff;
-  let material = new THREE.MeshBasicMaterial({
-    color: random_color,
-    wireframe: true,
-  });
-  vortex = new THREE.Mesh(geometry, material);
-  scene.add(vortex);
-
-  renderer = new THREE.CanvasRenderer();
-  renderer.setSize(WIDTH, HEIGHT);
-
-  document.getElementById("container").appendChild(renderer.domElement);
-
-  renderer.domElement.style.top = window.innerHeight - HEIGHT / 2 + "px";
-
-  window.addEventListener("resize", onWindowResize, false);
-}
-
-function onWindowResize() {
-  let WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight;
-
-  if (HEIGHT > WIDTH) {
-    HEIGHT = WIDTH;
-  }
-
-  camera.aspect = WIDTH / HEIGHT;
-  camera.updateProjectionMatrix();
-
-  WIDTH = WIDTH * 2;
-  HEIGHT = HEIGHT * 2;
-
-  renderer.domElement.style.top = window.innerHeight - HEIGHT / 2 + "px";
-
-  renderer.setSize(WIDTH, HEIGHT);
-}
-
-function render() {
-  let time = Date.now() * 0.005;
-
-  vortex.rotation.y = 0.02 * time;
-  vortex.rotation.z = 0.02 * time;
-
-  if (!diamond) {
-    let radius = 50,
-      segments = 20,
-      rings = 2;
-    let geometry = new THREE.SphereGeometry(radius, segments, rings);
-    diamond = new THREE.Mesh(geometry);
-    $diamond_color = diamond.material.color;
-    scene.add(diamond);
-  }
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
-}
-
-function diamond_color() {
-  if (!diamond) return;
-
-  let rand = Math.ceil(Math.random() * 50);
-
-  if (rand % 2 === 0) {
-    $diamond_color.setRGB(
-      Math.random() * 256,
-      Math.random() * 256,
-      Math.random() * 256
+    let radius = 100,
+      segments = 35,
+      rings = 18;
+    let geometry = new THREE.SphereGeometry(
+      radius,
+      segments,
+      rings,
+      0,
+      Math.PI * 2,
+      0,
+      Math.PI * 2
     );
+    let random_color = Math.random() * 0xffffff;
+    let material = new THREE.MeshBasicMaterial({
+      color: random_color,
+      wireframe: true,
+    });
+    this.vortex = new THREE.Mesh(geometry, material);
+    this.scene.add(this.vortex);
+
+    this.renderer = new THREE.CanvasRenderer();
+    this.renderer.setSize(this.WIDTH, this.HEIGHT);
+
+    document.getElementById("container").appendChild(this.renderer.domElement);
+
+    this.renderer.domElement.style.top =
+      window.innerHeight - this.HEIGHT / 2 + "px";
+
+    window.addEventListener("resize", this.onWindowResize, false);
+    this.randomizeSphereColor = Math.random() < 0.5;
+    this.randomizeDiamondColor = Math.random() < 0.5;
+
+    this.render();
+    this.diamond_color();
   }
 
-  requestAnimationFrame(diamond_color);
+  onWindowResize() {
+    let WIDTH = window.innerWidth,
+      HEIGHT = window.innerHeight;
+
+    if (HEIGHT > WIDTH) {
+      HEIGHT = WIDTH;
+    }
+
+    this.camera.aspect = WIDTH / HEIGHT;
+    this.camera.updateProjectionMatrix();
+
+    WIDTH = WIDTH * 2;
+    HEIGHT = HEIGHT * 2;
+
+    this.renderer.domElement.style.top = window.innerHeight - HEIGHT / 2 + "px";
+
+    this.renderer.setSize(WIDTH, HEIGHT);
+  }
+
+  render() {
+    let time = Date.now() * 0.005;
+
+    this.vortex.rotation.y = 0.02 * time;
+    this.vortex.rotation.z = 0.02 * time;
+
+    if (!this.diamond) {
+      let radius = 50,
+        segments = 20,
+        rings = 2;
+      let geometry = new THREE.SphereGeometry(radius, segments, rings);
+      this.diamond = new THREE.Mesh(geometry);
+      this.scene.add(this.diamond);
+    }
+
+    if (this.randomizeSphereColor) {
+      this.vortex.material.color.setRGB(
+        Math.round(Math.random()),
+        Math.round(Math.random()),
+        Math.round(Math.random())
+      );
+    }
+
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(() => this.render());
+  }
+
+  diamond_color() {
+    if (!this.diamond) return;
+
+    if (this.randomizeDiamondColor) {
+      this.diamond.material.color.setRGB(
+        Math.round(Math.random()),
+        Math.round(Math.random()),
+        Math.round(Math.random())
+      );
+    }
+
+    requestAnimationFrame(() => this.diamond_color());
+  }
 }
 
-init();
-requestAnimationFrame(render);
-requestAnimationFrame(diamond_color);
+new Sphere();
