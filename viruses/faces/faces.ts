3d8d522314a82eca5ecd51a6241f55b53c91262d
Michael Squashic <msquashic@gmail.com>
2023-01-22 17:45:59 -0500

    faces - blink wip

--- viruses/faces/faces.ts
+++ viruses/faces/faces.ts
@@ -1,1 +1,28 @@
 import "./faces.scss";
+
+class Faces {
+  eyes = document.querySelectorAll<HTMLElement>(".eye");
+
+  blink() {
+    return new Promise((resolve, reject) => {
+      this.eyes.forEach((el, key) => {
+        el.classList.add("blink");
+      });
+      setTimeout(() => {
+        this.eyes.forEach((el, key) => {
+          el.classList.remove("blink");
+        });
+      }, 200);
+      setTimeout(() => {
+        resolve(this);
+      }, 300);
+    });
+  }
+}
+
+const faces = new Faces();
+setInterval(() => {
+  setTimeout(() => {
+    faces.blink().then(() => faces.blink());
+  }, 1000);
+}, 2000);
