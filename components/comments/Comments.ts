e29b3f68b5a90d968672c4ca2d8ff8c507946b28
Michael Squashic <msquashic@gmail.com>
2023-03-15 22:24:38 -0400

    add some TODOs

--- components/comments/Comments.ts
+++ components/comments/Comments.ts
@@ -9,6 +9,8 @@ export default class Comments {
   template = document.getElementById("comment-template")!.innerHTML;
 
   constructor() {
+    // TODO: add loading animation
+    // TODO: only show comment form once per day
     this.loadComments();
     this.commentFormEl.addEventListener("submit", this.onSubmit.bind(this));
   }
@@ -16,6 +18,10 @@ export default class Comments {
   onSubmit(e: any) {
     e.preventDefault();
     const formData = new FormData(e.target);
+
+    // TODO: min num words validation
+    // TODO: max num words validation
+    // TODO: max char validation
 
     this.submitComment(formData).then((response) => {
       console.log("response", response);
